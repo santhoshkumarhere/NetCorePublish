401b584f54f810e47f4343f529c5dd3170001e3f
kumars9 <santhoshk.kumar@corteva.com>
2021-02-24 14:50:03 -0600

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace NetCorePublish.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Environment = Configuration.GetValue<string>("PublishEnvironment")
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+﻿dotnet publish /p:Configuration=Release /p:EnvironmentName=QA
+
+--LocalRun
+$Env:ASPNETCORE_ENVIRONMENT = "QA"
+dotnet run --no-launch-profile
